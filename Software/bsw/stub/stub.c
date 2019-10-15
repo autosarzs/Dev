30be6cb12563566e91b4981cd52acdab2af2a679
Mahmoud Saad <mahmoudmsaad2@gmail.com>
2019-10-16 00:11:27 +0200

    include ComStack_Types.h in stub.c resolve unkown type PduIdType

--- Software/bsw/stub/stub.c
+++ Software/bsw/stub/stub.c
@@ -33,6 +33,7 @@
  **                                                                            **
  *******************************************************************************/
 #include "Std_Types.h"
+#include "ComStack_Types.h"
 
 Std_ReturnType Det_ReportError(uint16 ModuleId, uint8 InstanceId, uint8 ApiId,
 		uint8 ErrorId) {
