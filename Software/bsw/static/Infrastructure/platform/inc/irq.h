50200de1d6a63e6736fa2f3cd2b99438351d9040
Mahmoud Saad <mahmoudmsaad2@gmail.com>
2019-10-16 18:57:08 +0200

    add header file for irq source

---
+++ Software/bsw/static/Infrastructure/platform/inc/irq.h
@@ -0,0 +1,44 @@
+/*******************************************************************************
+**                                                                            **
+**  Copyright (C) AUTOSarZs olc (2019)                                        **
+**                                                                            **
+**  All rights reserved.                                                      **
+**                                                                            **
+**  This document contains proprietary information belonging to AUTOSarZs     **
+**  olc . Passing on and copying of this document, and communication          **
+**  of its contents is not permitted without prior written authorization.     **
+**                                                                            **
+********************************************************************************
+**                                                                            **
+**  FILENAME     : irq.h                                                      **
+**                                                                            **
+**  VERSION      : 1.0.0                                                      **
+**                                                                            **
+**  DATE         : 2019-09-22                                                 **
+**                                                                            **
+**  VARIANT      : Variant PB                                                 **
+**                                                                            **
+**  PLATFORM     : TIVA C                                                     **
+**                                                                            **
+**  AUTHOR       : AUTOSarZs-DevTeam                                          **
+**                                                                            **
+**  VENDOR       : AUTOSarZs OLC                                              **
+**                                                                            **
+**                                                                            **
+**  DESCRIPTION  : interrupts header file                                     **
+**                                                                            **
+**  SPECIFICATION(S) : Specification of CAN Driver, AUTOSAR Release 4.3.1     **
+**                                                                            **
+**  MAY BE CHANGED BY USER : no                                               **
+**                                                                            **
+*******************************************************************************/
+
+
+#ifndef BSW_STATIC_INFRASTRUCTURE_PLATFORM_INC_IRQ_H_
+#define BSW_STATIC_INFRASTRUCTURE_PLATFORM_INC_IRQ_H_
+
+void irq_Enable(void);
+void irq_Disable(void);
+
+
+#endif /* BSW_STATIC_INFRASTRUCTURE_PLATFORM_INC_IRQ_H_ */
