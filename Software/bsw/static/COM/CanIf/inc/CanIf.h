57334f006a824f94857d952afb1de6bb3d8e1281
A-m-Tawfik <a.mohammed.tawfik@gmail.com>
2019-10-20 23:10:03 +0200

    adding empty CanIf.h and adding implemented CanIf_Types.h

---
+++ Software/bsw/static/COM/CanIf/inc/CanIf.h
@@ -0,0 +1,47 @@
+/*******************************************************************************
+**                                                                            **
+**  Copyright (C) AUTOSarZs olc (2019)		                                  **
+**                                                                            **
+**  All rights reserved.                                                      **
+**                                                                            **
+**  This document contains proprietary information belonging to AUTOSarZs     **
+**  olc . Passing on and copying of this document, and communication          **
+**  of its contents is not permitted without prior written authorization.     **
+**                                                                            **
+********************************************************************************
+**                                                                            **
+**  FILENAME     : CanIf			                                      **
+**                                                                            **
+**  VERSION      : 1.0.0                                                      **
+**                                                                            **
+**  DATE         : 2019-10-20                                                 **
+**                                                                            **
+**  VARIANT      : Variant PB                                                 **
+**                                                                            **
+**  PLATFORM     : TIVA C		                                              **
+**                                                                            **
+**  AUTHOR       : AUTOSarZs-DevTeam	                                      **
+**                                                                            **
+**  VENDOR       : AUTOSarZs OLC	                                          **
+**                                                                            **
+**                                                                            **
+**  DESCRIPTION  : CAN Interface source file                                  **
+**                                                                            **
+**  SPECIFICATION(S) : Specification of CAN Inteface, AUTOSAR Release 4.3.1   **
+**                                                                            **
+**  MAY BE CHANGED BY USER : no                                               **
+**                                                                            **
+*******************************************************************************/
+#ifndef __CANIF_H__
+#define __CANIF_H__
+
+
+
+
+
+
+
+
+
+
+#endif /* __CANIF_H__ */
