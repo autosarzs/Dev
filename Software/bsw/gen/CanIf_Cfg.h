f65abfee8aee153503f2e7319cc93a67a871237e
Mohammed Ghazy <moh1139.mg@gmail.com>
2019-11-23 21:41:12 +0200

    Completed CanIfTxPduCfg Container

--- Software/bsw/gen/CanIf_Cfg.h
+++ Software/bsw/gen/CanIf_Cfg.h
@@ -56,10 +56,10 @@
 //  Selects the desired software filter mechanism for reception only. Each
 //  implemented software filtering method is identified by this enumeration
 //  number.
-//  Range: BINARY
-//         INDEX
-//         LINEAR
-//         TABLE
+//  Range: 	BINARY Selects Binary Filter method.
+//			INDEX Selects Index Filter method.
+//			LINEAR Selects Linear Filter method.
+//			TABLE Selects Table Filter method.
 //	dependency: BasicCAN reception must be enabled by referenced
 //	parameter CAN_HANDLE_TYPE of the CAN Driver module via
 //	CANIF_HRH_HANDLETYPE_REF for at least one HRH.
@@ -278,12 +278,16 @@ Range: 0 .. 536870911 */
 #define CanIfTxPduCanIdMask1					(1<<(uint32)1)
 
 /* Type of CAN Identifier of the transmit CAN L-PDU used by the CAN
-Driver module for CAN L-PDU transmission. */
-typedef uint8 CanIfTxPduCanIdTypeConfig;
-#define EXTENDED_CAN							((CanIfTxPduCanIdTypeConfig)0x00)
-#define EXTENDED_FD_CAN							((CanIfTxPduCanIdTypeConfig)0x01)
-#define STANDARD_CAN							((CanIfTxPduCanIdTypeConfig)0x02)
-#define STANDARD_FD_CAN							((CanIfTxPduCanIdTypeConfig)0x03)
+Driver module for CAN L-PDU transmission. 
+Range: 	EXTENDED_CAN CAN 		frame with extended identifier (29 bits)
+		EXTENDED_FD_CAN CAN FD 	frame with extended identifier (29 bits)
+		STANDARD_CAN CAN 		frame with standard identifier (11 bits)
+		STANDARD_FD_CAN CAN FD 	frame with standard identifier (11 bits)*/
+typedef uint8 CANIF_TXPDU_CANID_TYPE;
+#define EXTENDED_CAN							((CANIF_TXPDU_CANID_TYPE)0x00)
+#define EXTENDED_FD_CAN							((CANIF_TXPDU_CANID_TYPE)0x01)
+#define STANDARD_CAN							((CANIF_TXPDU_CANID_TYPE)0x02)
+#define STANDARD_FD_CAN							((CANIF_TXPDU_CANID_TYPE)0x03)
 
 /* ECU wide unique, symbolic handle for transmit CAN L-SDU.
 Range: 0..max. number of CantTxPduIds */
@@ -295,7 +299,7 @@ If there is no CanIfTxPduPnFilterPdu configured per controller,
 the corresponding controller applies no CanIfPnFilter.
 dependency: This parameter shall only be configurable if CanIfPublicPnSupport equals True. */
 #if(CanIfPublicPnSupport==STD_ON)
-	#define CanIfTxPduPnFilterPduConfig			STD_OFF
+	#define CANIF_TXPDU_PNFILTER_PDU			STD_OFF
 #endif
 
 /* Enables and disables transmit confirmation for each transmit CAN
@@ -312,6 +316,118 @@ dependency: CANIF_READTXPDU_NOTIFY_STATUS_API must be enabled.*/
 	#define CANIF_TXPDU_READ_NOTIFYSTATUS		STD_OFF
 #endif
 
+/* Determines if or if not CanIf shall use the trigger transmit API for this PDU.
+dependency: If CanIfTxPduTriggerTransmit is TRUE then CanIfTxPduUserTxConfirmationUL 
+has to be either PDUR or CDD and CanIfTxPduUserTriggerTransmitName has to be specified accordingly */
+#define CANIF_TXPDU_TRIGGERTRANSMIT				STD_OFF
+
+/* Enables/disables truncation of PDUs that exceed the configured size. */
+#define CANIF_TXPDU_TRUNCATION					STD_ON
+
+/* Defines the type of each transmit CAN L-PDU.
+Range:	DYNAMIC 	CAN ID is defined at runtime.
+		STATIC 		CAN ID is defined at compile-time. */
+typedef uint8 CANIF_TXPDU_TYPE;
+#define DYNAMIC									((CANIF_TXPDU_TYPE)0x00U)
+#define STATIC									((CANIF_TXPDU_TYPE)0x01U)
+
+/* This parameter defines the upper layer (UL) module to which the confirmation of
+the successfully transmitted CANTXPDUID has to be routed via the <User_TxConfirmation>.
+This <User_TxConfirmation> has to be invoked when the confirmation of the configured CANTXPDUID
+will be received by a Tx confirmation event from the CAN Driver module.
+If no upper layer (UL) module is configured, no <User_TxConfirmation> has to be called in case of
+a Tx confirmation event of the CANTXPDUID from the CAN Driver module.
+Range:	CAN_NM		CAN NM
+		CAN_TP 		CAN TP
+		CAN_TSYN	Global Time Synchronization over CAN
+		CDD 		Complex Driver
+		J1939NM 	J1939Nm
+		J1939TP 	J1939Tp
+		PDUR 		PDU Router
+		XCP 		Extended Calibration Protocol 
+Note: If CanIfTxPduTriggerTransmit is not specified or FALSE, no upper
+layer modules have to be configured for Trigger Transmit. Therefore,
+<User_TriggerTransmit>() will not be called and CanIfTxPduUserTxConfirmationUL
+as well as CanIfTxPduUserTriggerTransmitName need not to be configured. */
+#if(CANIF_TXPDU_TRIGGERTRANSMIT==STD_ON)
+	typedef uint8 CanIfTxPduUserTxConfirmationUL;
+	#define CAN_NM									((CanIfTxPduUserTxConfirmationUL)0x00U)
+	#define CAN_TP									((CanIfTxPduUserTxConfirmationUL)0x01U)
+	#define CAN_TSYN								((CanIfTxPduUserTxConfirmationUL)0x02U)
+	#define CDD										((CanIfTxPduUserTxConfirmationUL)0x03U)
+	#define J1939NM									((CanIfTxPduUserTxConfirmationUL)0x04U)
+	#define J1939TP									((CanIfTxPduUserTxConfirmationUL)0x05U)
+	#define PDUR									((CanIfTxPduUserTxConfirmationUL)0x06U)
+	#define XCP										((CanIfTxPduUserTxConfirmationUL)0x07U)
+#endif
+
+/* This parameter defines the name of the <User_TriggerTransmit>. This
+parameter depends on the parameter CanIfTxPduUserTxConfirmationUL. If
+CanIfTxPduUserTxConfirmationUL equals CAN_TP, CAN_NM, PDUR, XCP, CAN_TSYN, J1939NM or J1939TP,
+the name of the <User_TriggerTransmit> is fixed. If CanIfTxPduUserTxConfirmationUL equals CDD,
+the name of the <User_TxConfirmation> is selectable.
+Please be aware that this parameter depends on the same parameter as CanIfTxPduUserTxConfirmationN
+It shall be clear which upper layer is responsible for that PDU.
+dependency: CanIfTxPduUserTriggerTransmitName requires CanIfTxPduUserTxConfirmationUL to be either PDUR or CDD.
+
+Note: If CanIfTxPduTriggerTransmit is not specified or FALSE, no upper
+layer modules have to be configured for Trigger Transmit. Therefore,
+<User_TriggerTransmit>() will not be called and CanIfTxPduUserTxConfirmationUL
+as well as CanIfTxPduUserTriggerTransmitName need not to be configured.
+
+[SWS_CANIF_00890] Configuration of <User_TriggerTransmit>(): If CanIfTxPduUserTxConfirmationUL
+is set to PDUR, CanIfTxPduUserTriggerTransmitName must be PduR_CanIfTriggerTransmit.
+
+[SWS_CANIF_00891] Configuration of <User_TriggerTransmit>(): If CanIfTxPduUserTxConfirmationUL
+is set to CDD, the name of the API <User_TriggerTransmit>() has to be configured via parameter CanIfTxPduUserTriggerTransmitName. */
+#if(CANIF_TXPDU_TRIGGERTRANSMIT==STD_ON)
+	#if(CanIfTxPduUserTxConfirmationUL==PDUR)
+		#define CanIfTxPduUserTriggerTransmitName	PduR_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_TP)
+		#define CanIfTxPduUserTriggerTransmitName	CANTP_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_NM)
+		#define CanIfTxPduUserTriggerTransmitName	CANNM_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==XCP)
+		#define CanIfTxPduUserTriggerTransmitName	XCP_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_TSYN)
+		#define CanIfTxPduUserTriggerTransmitName	CANTSYN_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==J1939NM)
+		#define CanIfTxPduUserTriggerTransmitName	J1939NM_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==J1939TP)
+		#define CanIfTxPduUserTriggerTransmitName	J1939TP_CanIfTriggerTransmit
+	#else if(CanIfTxPduUserTxConfirmationUL==CDD)
+		#define CanIfTxPduUserTriggerTransmitName	CDD_CanIfTriggerTransmit /* Selectable */
+	#endif	
+#endif
+
+/* This parameter defines the name of the <User_TxConfirmation>. This parameter depends
+on the parameter CanIfTxPduUserTxConfirmationUL. If CanIfTxPduUserTxConfirmationUL equals
+CAN_TP, CAN_NM, PDUR, XCP, CAN_TSYN, J1939NM or J1939TP, the name of the <User_TxConfirmation> is fixed.
+If CanIfTxPduUserTxConfirmationUL equals CDD, the name of the <User_TxConfirmation> is selectable.
+
+Note: If transmit confirmations are not necessary or no upper layer modules are configured
+for transmit confirmations and thus <User_TxConfirmation>() shall not be called, CanIfTxPduUserTxConfirmationUL
+and CanIfTxPduUserTxConfirmationName need not to be configured. */
+#if(CANIF_TXPDU_TRIGGERTRANSMIT==STD_ON)
+	#if(CanIfTxPduUserTxConfirmationUL==PDUR)
+		#define CanIfTxPduUserTxConfirmationName	PduR_CanIfTxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_NM)
+		#define CanIfTxPduUserTxConfirmationName	CANNM_TxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==J1939NM)
+		#define CanIfTxPduUserTxConfirmationName	J1939NM_TxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==J1939TP)
+		#define CanIfTxPduUserTxConfirmationName	J1939TP_TxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_TP)
+		#define CanIfTxPduUserTxConfirmationName	CANTP_TxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==XCP)
+		#define CanIfTxPduUserTxConfirmationName	XCP_CanIfTxConfirmation
+	#else if(CanIfTxPduUserTxConfirmationUL==CAN_TSYN)
+		#define CanIfTxPduUserTxConfirmationName	CANTSYN_CanIfTxConfirmation
+
+	#else if(CanIfTxPduUserTxConfirmationUL==CDD)
+		#define CanIfTxPduUserTxConfirmationName	CDD_CanIfTxConfirmation /* Selectable */
+	#endif	
+#endif
 
 
 
