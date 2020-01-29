9dacd3eb533ae48afaf810c35fbf79edce001c66
abdulmaguideissa <abdulmaguid.eissa@gmail.com>
2020-01-29 16:46:40 +0200

    Adding errors codes

--- Software/bsw/static/COM/CanIf/inc/CanIf.h
+++ Software/bsw/static/COM/CanIf/inc/CanIf.h
@@ -35,10 +35,85 @@
 #ifndef __CANIF_H__
 #define __CANIF_H__
 
+/* CanIf Vendor ID in AUTOSAR */
+#define CANIF_VENDOR_ID                    (2020U)
+
+/* CanIf Module ID in AUTOSAR */
+#define	CANIF_MODULE_ID					   (060U)
+
+/* CanIf Instance ID */
+#define CANIF_INSTANCE_ID                  (0U)
+
+/*
+ * SW Module Version is 1.0.0
+ */
+#define CANIF_SW_MAJOR_VERSION             (1U)
+#define CANIF_SW_MINOR_VERSION             (0U)
+#define CANIF_SW_PATCH_VERSION             (0U)
+
+ /*
+  * AUTOSAR Version is 4.3.1 compatible
+  */
+#define CANIF_AR_RELEASE_MAJOR_VERSION    (4U) 
+#define CANIF_AR_RELEASE_MINOR_VERSION    (3U)
+#define CANIF_AR_RELEASE_PATCH_VERSION    (1U)
+
+/******************************************************************************
+*                             DET Error Codes                                 *
+*******************************************************************************/
+/*
+    Development Errors.
+*/
+#define CANIF_E_PARAM_CANID                ((uint8)10)
+#define CANIF_E_PARAM_HOH                  ((uint8)12)
+#define CANIF_E_PARAM_LPDU                 ((uint8)13)
+#define CANIF_E_PARAM_CONTROLLERID         ((uint8)15)
+#define CANIF_E_PARAM_WAKEUPSOURCE         ((uint8)16)
+#define CANIF_E_PARAM_TRCV                 ((uint8)17)
+#define CANIF_E_PARAM_TRCVMODE             ((uint8)18)
+#define CANIF_E_PARAM_TRCVWAKEUPMODE       ((uint8)19)
+#define CANIF_E_PARAM_CTRLMODE             ((uint8)21)
+#define CANIF_E_PARAM_PDU_MODE             ((uint8)22)
+#define CANIF_E_PARAM_POINTER              ((uint8)20)
+#define CANIF_E_UNINIT                     ((uint8)30)
+#define CANIF_E_INVALID_TXPDUID            ((uint8)50)
+#define CANIF_E_INVALID_RXPDUID            ((uint8)60)
+#define CANIF_E_INIT_FAILED                ((uint8)80)
+/*
+    Runtime Errors.
+*/
+#define CANIF_E_INVALID_DATA_LENGTH        ((uint8)61)
+#define CANIF_E_DATA_LENGTH_MISMATCH       ((uint8)62)
+#define CANIF_E_STOPPED                    ((uint8)70)
+#define CANIF_E_TXPDU_LENGTH_EXCEEDED      ((uint8)90)
+
+
 #include "CanIf_Types.h"
 #include "CanIf_Cfg.h"
 
-Std_ReturnType CanIf_GetControllerErrorState(uint8 ControllerId, Can_ErrorStateType* ErrorStatePtr);
+
+/*******************************************************************************
+ *                           API Service ID Macros                             *
+********************************************************************************/
+#define CANIF_GET_CONTROLLER_ERROR_STATE_SID                   ((uint8)0x4b)
+
+/*******************************************************************************
+* Service Name:       CanIf_GetControllerErrorState
+* Service ID[hex]:    0x4b
+* Sync/Async:         Synchronous
+* Reentrancy:         Non Reentrant for the same ControllerId
+* Parameters (in):    ControllerId Abstracted CanIf ControllerId which is assigned to a
+                      CAN controller, which is requested for ErrorState.
+* Parameters (inout): None
+* Parameters (out):   ErrorStatePtr Pointer to a memory location, where the error state
+                      of the CAN controller will be stored.
+* Return value:       Std_ReturnType E_OK: Error state request has been accepted.
+                      E_NOT_OK: Error state request has not been accepted.
+* Description:        This service calls the corresponding CAN Driver service for 
+                      obtaining the error state of the CAN controller.
+***********************************************************************************/
+Std_ReturnType 
+CanIf_GetControllerErrorState(uint8 ControllerId, Can_ErrorStateType* ErrorStatePtr);
 
 
 #endif /* __CANIF_H__ */
