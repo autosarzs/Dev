8ffe3aa3433db15ab8fcead2c11bfa1cf903a2fc
abdulmaguideissa <abdulmaguid.eissa@gmail.com>
2020-01-31 19:01:01 +0200

    Adding module description macros

--- Software/bsw/static/COM/CanIf/inc/CanIf.h
+++ Software/bsw/static/COM/CanIf/inc/CanIf.h
@@ -35,10 +35,120 @@
 #ifndef __CANIF_H__
 #define __CANIF_H__
 
-#include "CanIf_Types.h"
-#include "CanIf_Cfg.h"
+/* CanIf Vendor ID in AUTOSAR */
+#define CANIF_VENDOR_ID                    (2020U)
 
-Std_ReturnType CanIf_GetPduMode(uint8 ControllerId, CanIf_PduModeType* PduModePtr);
+/* CanIf Module ID in AUTOSAR */
+#define	CANIF_MODULE_ID					   (060U)
+
+/* CanIf Instance ID */
+#define CANIF_INSTANCE_ID                  (0U)
+
+/*
+ * SW Module Version is 1.0.0
+ */
+#define CANIF_SW_MAJOR_VERSION             (1U)
+#define CANIF_SW_MINOR_VERSION             (0U)
+#define CANIF_SW_PATCH_VERSION             (0U)
+
+ /*
+  * AUTOSAR Version is 4.3.1 compatible
+  */
+#define CANIF_AR_RELEASE_MAJOR_VERSION    (4U) 
+#define CANIF_AR_RELEASE_MINOR_VERSION    (3U)
+#define CANIF_AR_RELEASE_PATCH_VERSION    (1U)
+
+/*
+    CanIf initialisation status codes
+*/
+#define CANIF_INITIALIZED                 (1U)
+#define CANIF_NOT_INITIALIZED             (0U)
+
+/******************************************************************************
+*                             DET Error Codes                                 *
+*******************************************************************************/
+/*
+    Development Errors.
+*/
+#define CANIF_E_PARAM_CANID                ((uint8)10)
+#define CANIF_E_PARAM_HOH                  ((uint8)12)
+#define CANIF_E_PARAM_LPDU                 ((uint8)13)
+#define CANIF_E_PARAM_CONTROLLERID         ((uint8)15)
+#define CANIF_E_PARAM_WAKEUPSOURCE         ((uint8)16)
+#define CANIF_E_PARAM_TRCV                 ((uint8)17)
+#define CANIF_E_PARAM_TRCVMODE             ((uint8)18)
+#define CANIF_E_PARAM_TRCVWAKEUPMODE       ((uint8)19)
+#define CANIF_E_PARAM_CTRLMODE             ((uint8)21)
+#define CANIF_E_PARAM_PDU_MODE             ((uint8)22)
+#define CANIF_E_PARAM_POINTER              ((uint8)20)
+#define CANIF_E_UNINIT                     ((uint8)30)
+#define CANIF_E_INVALID_TXPDUID            ((uint8)50)
+#define CANIF_E_INVALID_RXPDUID            ((uint8)60)
+#define CANIF_E_INIT_FAILED                ((uint8)80)
+  /*
+      Runtime Errors.
+  */
+#define CANIF_E_INVALID_DATA_LENGTH        ((uint8)61)
+#define CANIF_E_DATA_LENGTH_MISMATCH       ((uint8)62)
+#define CANIF_E_STOPPED                    ((uint8)70)
+#define CANIF_E_TXPDU_LENGTH_EXCEEDED      ((uint8)90)
+
+
+#include "CanIf_Types.h"
+  /* AUTOSAR version checking */
+#if ((CANIF_AR_RELEASE_MAJOR_VERSION != CANIF_TYPES_AR_RELEASE_MAJOR_VERSION)\
+ ||  (CANIF_AR_RELEASE_MINOR_VERSION != CANIF_TYPES_AR_RELEASE_MINOR_VERSION)\
+ ||  (CANIF_AR_RELEASE_PATCH_VERSION != CANIF_TYPES_AR_RELEASE_PATCH_VERSION))
+#error "The AR version of Det.h does not match the expected version"
+#endif /* AUTOSAR version checking */
+
+/* SW module version checking */
+#if ((CANIF_SW_MAJOR_VERSION != CANIF_TYPES_SW_MAJOR_VERSION)\
+ ||  (CANIF_SW_MINOR_VERSION != CANIF_TYPES_SW_MINOR_VERSION)\
+ ||  (CANIF_SW_PATCH_VERSION != CANIF_TYPES_SW_PATCH_VERSION))
+#error "The AR version of Det.h does not match the expected version"
+#endif /* SW module version checking */
+
+#include "CanIf_Cfg.h"
+/* AUTOSAR version checking */
+#if ((CANIF_AR_RELEASE_MAJOR_VERSION != CANIF_CFG_AR_RELEASE_MAJOR_VERSION)\
+ ||  (CANIF_AR_RELEASE_MINOR_VERSION != CANIF_CFG_AR_RELEASE_MINOR_VERSION)\
+ ||  (CANIF_AR_RELEASE_PATCH_VERSION != CANIF_CFG_AR_RELEASE_PATCH_VERSION))
+#error "The AR version of Det.h does not match the expected version"
+#endif /* AUTOSAR version checking */
+
+/* SW module version checking */
+#if ((CANIF_SW_MAJOR_VERSION != CANIF_CFG_SW_MAJOR_VERSION)\
+ ||  (CANIF_SW_MINOR_VERSION != CANIF_CFG_SW_MINOR_VERSION)\
+ ||  (CANIF_SW_PATCH_VERSION != CANIF_CFG_SW_PATCH_VERSION))
+#error "The AR version of Det.h does not match the expected version"
+#endif /* SW module version checking */
+
+
+/*******************************************************************************
+ *                           API Service ID Macros                             *
+********************************************************************************/
+typedef uint8 CanIf_ServiceId;
+#define CANIF_GET_PDU_MODE_SID                   ((CanIf_ServiceId)0x0a)
+
+/*******************************************************************************
+* Service Name:       CanIf_GetPduMode
+* Service ID[hex]:    0x0a
+* Sync/Async:         Synchronous
+* Reentrancy:         Reentrant (Not for the same channel)
+* Parameters (in):    ControllerId All PDUs of the own ECU connected to the corre-
+                      sponding CanIf ControllerId, which is assigned to a physical 
+                      CAN controller are addressed.
+* Parameters (inout): None
+* Parameters (out):   PduModePtr Pointer to a memory location, where the current
+                      mode of the logical PDU channel will be stored.                      
+* Return value:       Std_ReturnType E_OK: PDU mode request has been accepted
+                                     E_NOT_OK: PDU mode request has not been 
+                                     ac-cepted.                      
+* Description:        This service reports the current mode of a requested PDU channel.
+***********************************************************************************/
+Std_ReturnType 
+CanIf_GetPduMode(uint8 ControllerId, CanIf_PduModeType* PduModePtr);
 
 
 #endif /* __CANIF_H__ */
