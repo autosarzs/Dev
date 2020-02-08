8e4ec8e68a6f1ac87acea89a9fce2f4f50380e5a
ahmadfayed7 <ahmadfayed7@gmail.com>
2020-02-09 01:50:22 +0200

    fix errors

--- Software/bsw/static/COM/CanIf/inc/CanIf.h
+++ Software/bsw/static/COM/CanIf/inc/CanIf.h
@@ -35,14 +35,21 @@
 #ifndef __CANIF_H__
 #define __CANIF_H__
 
-#include "CanIf_Types.h"
+
 #include "CanIf_Cfg.h"
 #include "Can.h"
 #include "Can_GeneralTypes.h"
 #include "Platform_Types.h"
 #include "CanIf_Types.h"
+typedef uint8 CanIf_PduModeType;
+#define CANIF_OFFLINE           ((CanIf_PduModeType)0x00U)
+#define CANIF_TX_OFFLINE        ((CanIf_PduModeType)0x01U)
+#define CANIF_TX_OFFLINE_ACTIVE ((CanIf_PduModeType)0x02U)
+#define CANIF_ONLINE            ((CanIf_PduModeType)0x03U)
 
+#define MAX_PDU_REQUEST  4
 
+Std_ReturnType CanIf_SetPduMode(uint8 ControllerId,CanIf_PduModeType PduModeRequest);
 Std_ReturnType CanIf_ReadRxPduData(PduIdType CanIfRxSduId,PduInfoType* CanIfRxInfoPtr);
 
 #endif /* __CANIF_H__ */
--- Software/bsw/static/COM/CanIf/src/CanIf.c
+++ Software/bsw/static/COM/CanIf/src/CanIf.c
@@ -32,32 +32,89 @@
 **  MAY BE CHANGED BY USER : no                                               **
 **                                                                            **
 *******************************************************************************/
-#include "..\inc\CanIf.h"
+#include "CanIf.h"
 
-static Canif_configType * Global_Config = NULL;
+static CanIf_ConfigType * Global_Config= NULL ;
 
-Std_ReturnType CanIf_ReadRxPduData(PduIdType CanIfRxSduId,PduInfoType* CanIfRxInfoPtr)
+
+Std_ReturnType CanIf_SetPduMode(uint8 ControllerId,CanIf_PduModeType PduModeRequest)
+{
+    Can_ControllerStateType Loc_Controller_Mode;
+    Std_ReturnType Loc_CanIf_SetPduMode_Ret = E_OK ;
+
+/*[SWS_CANIF_00341] If CanIf_SetPduMode() is called with invalid
+ControllerId, CanIf shall report development error code
+CANIF_E_PARAM_CONTROLLERID to the Det_ReportError service of the
+DET module. c(SRS_BSW_00323)*/
+
+
+ if (ControllerId > 5 )
+        {
+   #if ( DevError == STD_ON )
+            Det_ReportError (CANIF_MODULE_ID,CANIF_INSTANSE_ID , CanIf_SetPduMode, CANIF_E_PARAM_CONTROLLERID );
+   #else
+            Loc_CanIf_SetPduMode_Ret = E_NOT_OK;
+   #endif
+        }
+
+
+/*[SWS_CANIF_00860]  If CanIf_SetPduMode() is called with invalid PduModeRequest,
+CanIf shall report development error code CANIF_E_PARAM_PDU_MODE
+to the Det_ReportError service of the DET module. c(SRS_BSW_00323)*/
+
+
+ if (PduModeRequest >= MAX_PDU_REQUEST  )
+        {
+   #if ( DevError == STD_ON )
+            Det_ReportError (CANIF_MODULE_ID,CANIF_INSTANSE_ID , CanIf_SetPduMode, CANIF_E_PARAM_PDU_MODE );
+   #else
+            Loc_CanIf_SetPduMode_Ret = E_NOT_OK;
+   #endif
+        }
+
+/*[SWS_CANIF_00874] The service CanIf_SetPduMode() shall not accept any request
+and shall return E_NOT_OK, if the controller mode referenced by ControllerId
+is not in state CAN_CS_STARTED. c()*/
+
+Loc_CanIf_SetPduMode_Ret==CanIf_GetControllerMode(ControllerId,&Loc_Controller_Mode );  /* To Get The mode of ControllerMode */
+
+if (Loc_CanIf_SetPduMode_Ret == E_OK )
+{
+    if (Loc_Controller_Mode != CAN_CS_STARTED )
+         return E_NOT_OK;
+}
+else
+{
+        return E_NOT_OK;
+}
+
+
+}
+
+
+Std_ReturnType CanIf_ReadRxPduData(PduIdType CanIfRxSduId,PduInfoType*CanIfRxInfoPtr)
 {
 Std_ReturnType Loc_CanIf_ReadRxPduData_Ret =E_OK ;
-can_ControllerStateType Loc_Controller_Mode;
+Can_ControllerStateType Loc_Controller_Mode;
 uint8 Loc_Controller_Id;
 /*[SWS_CANIF_00324] d The function CanIf_ReadRxPduData() shall not accept a
 request and return E_NOT_OK, if the corresponding controller mode refrenced by Con�trollerId is different to CAN_CS_STARTED and the channel mode is in the receive
 path online.
 */
 
-Loc_Controller_Id=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduHrhIdRef.CanIfCtrlId;
-Loc_CanIf_ReadRxPduData_Ret=CanIf_GetControllerMode(Loc_Controller_Id,&Loc_ControllerMode );
+Loc_Controller_Id=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduHrhIdRef->CanIfHrhCanCtrlIdRef->CanIfCtrlId;
+
+Loc_CanIf_ReadRxPduData_Ret=CanIf_GetControllerMode(Loc_Controller_Id,&Loc_Controller_Mode );
 if (Loc_CanIf_ReadRxPduData_Ret==E_OK )
     {
-    if (Loc_Controller_Mode != CAN_CS_Started )
+    if (Loc_Controller_Mode != CAN_CS_STARTED )
          return E_NOT_OK;
-        }
+
     }
     else
     {
         return E_NOT_OK;
-        }
+    }
   /*
 [SWS_CANIF_00325] d If parameter CanIfRxSduId of CanIf_ReadRxPduData()
 has an invalid value, e.g. not configured to be stored within CanIf via
@@ -72,7 +129,7 @@ if (CanIfRxSduId >= RX_CAN_L_PDU_NUM )
    #else
             Loc_CanIf_ReadRxPduData_Ret=E_NOT_OK;
    #endif
-            }
+          }
 /*[SWS_CANIF_00326] d If parameter CanIfRxInfoPtr of
 CanIf_ReadRxPduData() has an invalid value, CanIf shall report develop�ment error code CANIF_E_PARAM_POINTER to the Det_ReportError service of
 the DET module, when CanIf_ReadRxPduData() is called*/
@@ -83,15 +140,15 @@ the DET module, when CanIf_ReadRxPduData() is called*/
    #else
             Loc_CanIf_ReadRxPduData_Ret=E_NOT_OK;
    #endif
-            }
+         }
   /* [SWS_CANIF_00329] d CanIf_ReadRxPduData() shall not be used for CanIfRxS�duId, which are defined to receive multiple CAN-Ids (Srange reception). c()
 Note: During the call of CanIf_ReadRxPduData() the buffer of CanIfRxInfoPtr is
 controlled by CanIf and this buffer should not be accessed for read/write from another
 call context. After return of this call the ownership changes to the upper layer*/
 
-   CanIfRxInfoPtr->SduDataPtr=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduBuf;  //will be defined in
+  // CanIfRxInfoPtr->SduDataPtr=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduBuf;  //will be defined in
 
-       CanIfRxInfoPtr->CanIfRxSduId=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduDataLength;
-
+    CanIfRxInfoPtr->SduLength=Global_Config->CanIfInitCfgObj.CanIfRxPduCfgObj[CanIfRxSduId].CanIfRxPduDataLength;
+    return Loc_CanIf_ReadRxPduData_Ret;
 
  }
