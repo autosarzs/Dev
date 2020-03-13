3dc1e23ab31d948a67da60b60624a7353b16fa13
Mohammed Ghazy <moh1139.mg@gmail.com>
2020-03-14 00:24:22 +0200

    Committed the conflicts resolved from merging down: - Implementation of CanIf_GetPduMode, CanIf_SetControllerMode with all it's changes

--- Software/bsw/static/COM/CanIf/src/CanIf.c
+++ Software/bsw/static/COM/CanIf/src/CanIf.c
@@ -67,10 +67,229 @@
  and also CANIF will be in CANIF_UNINT.
  CANIF_READY  : The function CanIf_Init shall change the module state to CANIF_READY
 ******************************************************************************/
-STATIC CanIf_ModuleStateType CanIf_ModuleState = CANIF_UNINT;
+
+static CanIf_ModuleStateType CanIf_ModuleState = CANIF_UNINT;
+
+static CanIf_ControllerStateType CanIf_ControllerState[CANIF_CONTROLLERS_NUM];
+
+/* CanIf PDU current mode. Initially, CANIF_OFFLINE [SWS_CANIF_00864]. */
+static CanIf_PduModeType CanIf_PduMode[CANIF_CONTROLLERS_NUM] = {CANIF_OFFLINE};// Init them all by CANIF_OFFLINE as default is CANIF_OFFLINE
+
+/* a pointer to the CanIf_ConfigType main Structure for the module to work on */
+static CanIf_ConfigType* CanIf_ConfigPtr = NULL_PTR;
+
+/*******************************************************************************
+*           CAN, CAN IF Controller ID Mapping Functions Declaration            *
+*******************************************************************************/
+static uint8 CanIf_GetCanController_Mapping(uint8 CanIfControllerId);
+static uint8 Can_GetCanIfController_Mapping(uint8 CanCtrlId, uint8 CanDrvId);
+
+/*******************************************************************************
+*           CAN, CAN IF Controller ID Mapping Functions Definition             *
+*******************************************************************************/
+static uint8 CanIf_GetCanController_Mapping(uint8 CanIfControllerId)// has CanController logical Id and want to get the physical Id
+{
+	uint8 ret = 255;
+	for(uint8 CanDrvNum = 0; CanDrvNum < CAN_DRIVER_NUM; CanDrvNum++)
+	{
+		for(uint8 CanIfCtrlNum = 0; CanIfCtrlNum < CANIF_CONTROLLERS_NUM; CanIfCtrlNum++)
+		{
+			if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CanDrvNum].CanIfCtrlCfgObj[CanIfCtrlNum].CanIfCtrlId) == CanIfControllerId)
+			{
+				ret = (CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CanDrvNum].CanIfCtrlCfgObj[CanIfCtrlNum].CanIfCtrlCanCtrlRef->CanControllerId);
+				break;
+			}
+		}
+	}
+	/*
+	uint8 ret = 255;
+	switch(CanIfControllerId)
+	{
+		case CANIF_CTRL_ID_0:
+		if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId) == CanIfControllerId)
+		{
+			ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId;
+		}
+		else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId) == CanIfControllerId)
+		{
+			ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId;
+		}
+		else
+		{
+			
+		}
+		break;
+		case CANIF_CTRL_ID_1:
+		if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId) == CanIfControllerId)
+		{
+			ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId;
+		}
+		else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId) == CanIfControllerId)
+		{
+			ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId;
+		}
+		else
+		{
+			
+		}
+		break;
+		default:
+		break;
+	}
+	return ret;
+	*/
+	return ret;
+}
+
+/* any Can Driver that uses this Function (Like: CanIf_RxIndication) will have the Can Driver Id that will be passed to this
+Mapping function to get the Logical CanIfController Id */
+static uint8 Can_GetCanIfController_Mapping(uint8 CanCtrlId, uint8 CanDrvId)// has CanController physical Id and want to get the logical Id
+{
+	uint8 ret = 255;
+	for(uint8 CanIfCtrlNum = 0; CanIfCtrlNum < CANIF_CONTROLLERS_NUM; CanIfCtrlNum++)
+	{
+		if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CanDrvId].CanIfCtrlCfgObj[CanIfCtrlNum].CanIfCtrlCanCtrlRef->CanControllerId) == CanCtrlId)
+		{
+			ret = (CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CanDrvId].CanIfCtrlCfgObj[CanIfCtrlNum].CanIfCtrlId);
+			break
+		}
+	}
+	/*
+	uint8 ret = 255;
+	switch(CanDrvId)
+	{
+		case CAN0_ID:
+		switch(CanCtrlId)
+		{
+			case CONTROLLER_0_ID:
+			if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_0_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId;
+			}
+			else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_0_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId;
+			}
+			break;
+			case CONTROLLER_1_ID:
+			if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_1_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId;
+			}
+			else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_1_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN0_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId;
+			}
+			break;
+			default:
+			break;
+		}
+		break;
+		case CAN1_ID:
+		switch(CanCtrlId)
+		{
+			case CONTROLLER_0_ID:
+			if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_0_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId;
+			}
+			else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_0_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId;
+			}
+			break;
+			case CONTROLLER_1_ID:
+			if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_1_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_0].CanIfCtrlId;
+			}
+			else if((CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlCanCtrlRef->CanControllerId)==CONTROLLER_1_ID)
+			{
+				ret = CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[CAN1_ID].CanIfCtrlCfgObj[CANIF_CTRL_ID_1].CanIfCtrlId;
+			}
+			break;
+			default:
+			break;
+		}
+		break;
+		default:
+		break;
+	}
+	return ret;
+	*/
+	return ret;
+}
 
 /*******************************************************************************
 *                    Functions Definitions                                     *
+
+*******************************************************************************/
+/*                  CanIf_SetPduMode service definition                       */
+Std_ReturnType 
+CanIf_SetPduMode(uint8 ControllerId, CanIf_PduModeType PduModeRequest)
+{
+	uint8 CanCtrlId = CanIf_GetCanController_Mapping(ControllerId);
+	if(CanIf_ControllerState[CanCtrlId]==CAN_CS_STARTED)
+	{
+		/* Implementation here */
+		// CanIf_ConfigPtr->CanIfCtrlDrvCfgObj[ControllerId].CanIfCtrlCfgObj[ControllerId].CanIfCtrlCanCtrlRef->CanControllerId;
+		// /* Pass the current PDU mode to PduModePtr */
+		// *PduModePtr = CanIf_PduMode;
+	}
+	else
+	{
+		
+	}
+	
+	
+    CanIf_PduMode = PduModeRequest;
+    /* Implementation here */
+}
+
+/*******************************************************************************/
+/*                  CanIf_GetPduMode service definition                        */
+Std_ReturnType 
+CanIf_GetPduMode(uint8 ControllerId, CanIf_PduModeType* PduModePtr)
+{
+    Std_ReturnType ret_status = E_OK;
+
+    /* Report errors */
+#if (CANIF_DEV_ERROR_DETECT == STD_ON)
+
+    /* [SWS_CANIF_00661] */
+    if (CANIF_UNINT == CanIf_ModuleState)
+    {
+        Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID,
+
+            CANIF_GET_PDU_MODE_SID, CANIF_E_UNINIT);
+        ret_status = E_NOT_OK;
+    }
+
+    /* [SWS_CANIF_00346] */
+    if (ControllerId >= CANIF_CONTROLLERS_NUM)
+    {
+        Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID,
+
+            CANIF_GET_PDU_MODE_SID, CANIF_E_PARAM_CONTROLLERID);
+        ret_status = E_NOT_OK;
+    }
+
+    /* [SWS_CANIF_00657]  */
+    if (NULL_PTR == PduModePtr)
+    {
+        Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID,
+
+            CANIF_GET_PDU_MODE_SID, CANIF_E_PARAM_POINTER);
+        ret_status = E_NOT_OK;
+    }
+#endif /* (CANIF_DEV_ERROR_DETECT == STD_ON) */
+
+
+    /* Pass the current PDU mode to PduModePtr */
+    *PduModePtr = CanIf_PduMode[ControllerId];
+    return ret_status;
+}
+
 ********************************************************************************/
 /*            CanIf_GetControllerErrorState service definition                 */
 Std_ReturnType 
@@ -89,7 +308,7 @@ CanIf_GetControllerErrorState(uint8 ControllerId, Can_ErrorStateType* ErrorState
 		ret_status= E_NOT_OK;
     }
     /* [SWS_CANIF_00898] */
-    if (ControllerId >= USED_CONTROLLERS_NUMBER)
+    if (ControllerId >= CANIF_CONTROLLERS_NUM)
     {
         Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID,
             CANIF_GET_CONTROLLER_ERROR_STATE_SID, CANIF_E_PARAM_CONTROLLERID);
