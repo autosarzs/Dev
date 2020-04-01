d2d985a43553e0a41635a9cc1f6ec4359d32b410
Muhammed-ELGendy <muhammedelgendy2020@gmail.com>
2020-04-01 23:35:40 +0200

    Editing

---
+++ Software/bsw/gen/J1939Tp_Cbk.h
@@ -0,0 +1,18 @@
+#ifndef __J1939TP_CBK_H__
+#define __J1939TP_CBK_H__
+
+#include "Can_GeneralTypes.h"
+#include "ComStack_Types.h"
+
+void J1939Tp_TxConfirmation(
+PduIdType TxPduId,
+Std_ReturnType result
+);
+
+void J1939Tp_RxIndication(
+PduIdType RxPduId,
+const PduInfoType* PduInfoPtr
+);
+
+
+#endif //__J1939TP_CBK_H__
