b304a0bb4236b720118e3b9dfec41bbc99035cd2
Muhammed-ELGendy <muhammedelgendy2020@gmail.com>
2020-04-20 00:35:30 +0200

    some enhancement

--- Software/bsw/static/COM/CanIf/src/CanIf.c
+++ Software/bsw/static/COM/CanIf/src/CanIf.c
@@ -113,9 +113,9 @@ CanIf_NotifStatusType CanIf_GetTxConfirmationState(uint8 ControllerId){
     if(ControllerId <= CANIF_CTRL_ID){
          notify = controllerData[ControllerId].transmit_confirmed;/* this value is modified by another func*/
     }else{
-        if(CANIF_DEV_ERROR_DETECT == STD_ON){
-           Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CANIF_GET_TX_CONFIRMATIONSTATE_ID, CANIF_E_PARAM_CONTROLLERID);
-        }
+		#if(CANIF_DEV_ERROR_DETECT == STD_ON)
+			Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CANIF_GET_TX_CONFIRMATIONSTATE_ID, CANIF_E_PARAM_CONTROLLERID);
+		#endif
     }
     return notify;
 }
