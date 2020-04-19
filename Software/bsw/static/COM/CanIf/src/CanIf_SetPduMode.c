356cbcdb87b3cb840aa7d085388cd317f69a5c79
ahmadfayed7 <ahmadfayed7@gmail.com>
2020-04-19 22:37:20 +0200

    divede two function to  two files

--- Software/bsw/static/COM/CanIf/src/CanIf_SetPduMode.c
+++ Software/bsw/static/COM/CanIf/src/CanIf_SetPduMode.c
@@ -34,19 +34,7 @@
  *******************************************************************************/
 #include "../inc/CanIf.h"
 
-/*
- *  Type Description : Struct to save recieved  PDUs (in case of enable CanIf_ReadRxPduData API un configuration) .
- */
-typedef struct
-{
-   PduInfoType *     PduInfoPtr;
-   PduIdType      PduId;
-}str_MapRXPdu ;
 
-static str_MapRXPdu  MapRXPdu[RX_CAN_L_PDU_NUM] = {0};
-/*Pointer to save configuration parameters set */
-static CanIf_ConfigType*    CanIf_ConfigPtr = NULL;
-CanIf_PduModeType CanIf_PduMode[CANIF_CONTROLLERS_NUM] ;
 
 Std_ReturnType CanIf_SetPduMode(uint8 ControllerId,CanIf_PduModeType PduModeRequest)
 {
@@ -164,75 +152,6 @@ return Loc_CanIf_SetPduMode_Ret;
 }
 
 
-Std_ReturnType CanIf_ReadRxPduData(PduIdType CanIfRxSduId,PduInfoType*CanIfRxInfoPtr)
-{
-    Std_ReturnType Loc_CanIf_ReadRxPduData_Ret =E_OK ;
-    Can_ControllerStateType Loc_Controller_Mode=0;
-    uint8 Loc_Controller_Id;
-    uint8 PDUCounter=0;
-    /*[SWS_CANIF_00324] d The function CanIf_ReadRxPduData() shall not accept a
-request and return E_NOT_OK, if the corresponding controller mode refrenced by Con�trollerId is different to CAN_CS_STARTED and the channel mode is in the receive
-path online.
-     */
 
 
-      Loc_Controller_Id= CanIf_ConfigPtr->CanIfInitCfgRef->CanIfRxPduCfgRef[CanIfRxSduId].CanIfRxPduHrhIdRef->CanIfHrhCanCtrlIdRef->CanIfCtrlCanCtrlRef->CanControllerId;
-    Loc_CanIf_ReadRxPduData_Ret=CanIf_GetControllerMode(Loc_Controller_Id,&Loc_Controller_Mode );
-    if (Loc_CanIf_ReadRxPduData_Ret==E_OK )
-    {
-        if (Loc_Controller_Mode != CAN_CS_STARTED )
-            return E_NOT_OK;
-
-    }
-    else
-    {
-        return E_NOT_OK;
-    }
-    /*
-[SWS_CANIF_00325] d If parameter CanIfRxSduId of CanIf_ReadRxPduData()
-has an invalid value, e.g. not configured to be stored within CanIf via
-CANIF_READRXPDU_DATA (ECUC_CanIf_00600), CanIf shall report development
-error code CANIF_E_INVALID_RXPDUID to the Det_ReportError service of the
-DET, when CanIf_ReadRxPduData() is called
-     */
-    if (CanIfRxSduId >= RX_CAN_L_PDU_NUM )
-    {
-#if ( DevError == STD_ON )
-        Det_ReportError (CANIF_MODULE_ID,CANIF_INSTANSE_ID , CanIf_ReadRxPduData_Id,CanIF_E_INVALID_RXPDUID );
-#else
-        Loc_CanIf_ReadRxPduData_Ret=E_NOT_OK;
-#endif
-    }
-    /*[SWS_CANIF_00326] d If parameter CanIfRxInfoPtr of
-CanIf_ReadRxPduData() has an invalid value, CanIf shall report develop�ment error code CANIF_E_PARAM_POINTER to the Det_ReportError service of
-the DET module, when CanIf_ReadRxPduData() is called*/
-    if (CanIfRxInfoPtr== NULL)
-    {
-#if ( DevError == STD_ON )
-        Det_ReportError (CANIF_MODULE_ID,CANIF_INSTANSE_ID , CanIf_ReadRxPduData_Id,CanIF_E_PARAM_POINTER );
-#else
-        Loc_CanIf_ReadRxPduData_Ret=E_NOT_OK;
-#endif
-    }
-    /* [SWS_CANIF_00329] d CanIf_ReadRxPduData() shall not be used for CanIfRxS�duId, which are defined to receive multiple CAN-Ids (Srange reception). c()
-Note: During the call of CanIf_ReadRxPduData() the buffer of CanIfRxInfoPtr is
-controlled by CanIf and this buffer should not be accessed for read/write from another
-call context. After return of this call the ownership changes to the upper layer*/
-  for(PDUCounter=0;PDUCounter<RX_CAN_L_PDU_NUM;PDUCounter++)
-  {
-      if(MapRXPdu->PduId==CanIfRxSduId)
-      {
-          CanIfRxInfoPtr->SduDataPtr=MapRXPdu->PduInfoPtr->SduDataPtr;
-          CanIfRxInfoPtr->SduLength=MapRXPdu->PduInfoPtr->SduLength;
-                  break;
-      }
-  }
-    return Loc_CanIf_ReadRxPduData_Ret;
-
-}
-//Stub
-Std_ReturnType CanIf_GetControllerMode(uint8 ControllerId,Can_ControllerStateType*ControllerModePtr)
-{
-    return E_OK;
-}
 
