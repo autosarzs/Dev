d0d797dfc9012027fe3e29822deaedf926817ee3
Sahar <sa>
2020-04-19 10:44:21 +0200

    Added files

--- Software/bsw/static/COM/CanIf/src/CanIf_Init.c
+++ Software/bsw/static/COM/CanIf/src/CanIf_Init.c
@@ -112,3 +112,120 @@ static CanIf_ModuleStateType CanIf_ModuleState = CANIF_UNINT;
 /*****************************************************************************************/
 
 
+/****************************************************************************************/
+ /*    Function Description    : This service Initializes internal and external          */
+ /*                              interfaces of the CAN Interface for the further         */
+ /*                              processing                                              */
+ /*    Parameter in            : ConfigPtr                                               */
+ /*    Parameter inout         : none                                                    */
+ /*    Parameter out           : none                                                    */
+ /*    Return value            : none                                                    */
+ /*    Requirement             : SWS_CANIF_00001                                          */
+ /*    Notes                   : All underlying CAN controllers and transceivers still   */
+ /*                              remain not operational.                                 */
+ /*                              The service CanIf_Init() is called only by the EcuM.    */
+ /*                              [SWS_CANIF_00857]  CanIf_Init() (see [SWS_CANIF_00085]) */
+ /*                              initializes the CanIds of the dynamic Transmit L-PDUs   */
+ /*                              with CanIfTxPduType to the value configured via         */
+ /*                              CanIfTxPduCanId.                                        */
+ /*                              [SWS_CANIF_00387]  When function CanIf_Init() is called,*/
+ /*                              CanIf shall initialize every Transmit L-PDU Buffer      */
+ /*                              assigned to CanIf.The requirement [SWS_CANIF_00387] is  */
+ /*                              necessary to prevent transmission of old data after     */
+ /*                              restart of the CAN Controller                           */
+ /*                              [SWS_CANIF_00864]  During initialization CanIf shall    */
+ /*                              switch every channel to CANIF_OFFLINE                   */
+ /*****************************************************************************************/
+ void CanIf_Init( const CanIf_ConfigType* ConfigPtr )
+ {
+     PduIdType counter ,DynamicPduCounter = 0;
+     uint8 TxBufferSize = 0;
+     uint8 PduCounter = 0;
+
+    /*Report Null pointer parameter error*/
+    #if (CANIF_DEV_ERROR_DETECT == STD_ON)
+     if(ConfigPtr == NULL_PTR)
+     {
+         Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID,CANIF_INIT_API_ID,CANIF_E_PARAM_POINTER);
+     }
+    #endif
+
+     /*Save in the global config*/
+     CanIf_ConfigPtr = ConfigPtr ;
+     /*
+      * Initialize the link time containers
+      */
+     /*Reference to the TX PDUs array*/
+     CanIfTxPduCfgPtr = CanIf_ConfigPtr->CanIfInitCfgRef->CanIfTxPduCfgRef;
+     /*Reference to the TR PDUs array*/
+     CanIfRxPduCfgPtr = CanIf_ConfigPtr->CanIfInitCfgRef->CanIfRxPduCfgRef ;
+     /*Reference to the CanIf_HRHs array*/
+     CanIfHrhCfgPtr   = CanIf_ConfigPtr->CanIfInitCfgRef->CanIfInitHohCfgRef->CanIfHrhCfgRef ;
+     /*Reference to the CanIf_HTHs array*/
+     CanIfHthCfgPtr   = CanIf_ConfigPtr->CanIfInitCfgRef->CanIfInitHohCfgRef->CanIfHthCfgRef ; ;
+
+     /*
+      * [SWS_CANIF_00857]  CanIf_Init() (see [SWS_CANIF_00085]) initializes the CanIds of
+      *  the dynamic Transmit L-PDUs with CanIfTxPduType to the value configured
+      *  via CanIfTxPduCanId.
+      */
+     for(counter = 0 ; counter<TX_CAN_L_PDU_NUM;counter++)
+     {
+         /*Check L-PDUs of dynamic type */
+         if(CanIf_ConfigPtr->CanIfInitCfgRef->CanIfTxPduCfgRef[counter].CanIfTxPduType == DYNAMIC_TX_PDU)
+         {
+             /*Save CanId init value as CanIfTxPduCanId */
+             MapCanIdToPdu[DynamicPduCounter].CanId = CanIf_ConfigPtr->CanIfInitCfgRef->CanIfTxPduCfgRef[counter] \
+                                                      .CanIfTxPduCanId ;
+             /*Save PduId assigned to this CanId */
+             MapCanIdToPdu[DynamicPduCounter].PduId = counter ;
+             DynamicPduCounter++;
+         }
+     }
+
+     /*
+      * Init Tx buffers only if transmit buffering is enabled
+      */
+      #if(CANIF_PUBLIC_TX_BUFFERING==STD_ON)
+
+        /* [SWS_CANIF_00387]  When function CanIf_Init() is called, CanIf shall initialize
+         * every Transmit L-PDU Buffer assigned to CanIf.
+         * The requirement [SWS_CANIF_00387] is necessary to prevent transmission of old data
+         *  after restart of the CAN Controller
+         */
+         for(counter = 0 ; counter<BUFFERS_NUM;counter++)
+         {
+             /*Get number of PDUs saved in this buffer*/
+              TxBufferSize = CanIf_ConfigPtr->CanIfPduTxBufferCfgRef[counter].CanIfBufferRef->CanIfBufferSize;
+
+             /*Loop to initialize all PDUs saved in this buffer */
+             for(PduCounter =0;PduCounter<TxBufferSize;PduCounter++)
+             {
+                /*
+                 * Initialize all PDUs information
+                 */
+                 /*Init dynamic CanId with 0*/
+                 CanIf_ConfigPtr->CanIfPduTxBufferCfgRef[counter].CanIfPduInfoRef[PduCounter].CanId   = 0 ;
+                 /*Init PDU Id with 0*/
+                 CanIf_ConfigPtr->CanIfPduTxBufferCfgRef[counter].CanIfPduInfoRef[PduCounter].TxPduId = 0 ;
+                 /*Init all sduDataBuffer elements with 0 by casting the array address as uint64* */
+                 *((uint64*)CanIf_ConfigPtr->CanIfPduTxBufferCfgRef[counter].CanIfPduInfoRef[PduCounter].SduDatabuffer)= 0;
+                 /*Init SduLength with 0 */
+                 CanIf_ConfigPtr->CanIfPduTxBufferCfgRef[counter].CanIfPduInfoRef[PduCounter].SduLength = 0 ;
+             }
+         }
+     #endif
+    /*
+     * [SWS_CANIF_00864]  During initialization CanIf shall switch every channel to CANIF_OFFLINE
+     */
+     for( counter=0; counter<CANIF_CONTROLLERS_NUM; counter++)
+     {
+         CanIf_PduMode[counter] = CANIF_OFFLINE ;
+     }
+
+     /*Set module to Ready state*/
+     CanIf_ModuleState = CANIF_READY ;
+ }
+
+
+
