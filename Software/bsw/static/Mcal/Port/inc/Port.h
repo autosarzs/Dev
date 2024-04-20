9bd7c5c2f6902a33981b1728e277a4ad2ff6ba32
Mustafa Bahaa <mustafa.bahaa123@gmail.com>
2024-04-20 13:27:09 +0200

    Add Port implementation

--- Software/bsw/static/Mcal/Port/inc/Port.h
+++ Software/bsw/static/Mcal/Port/inc/Port.h
@@ -43,4 +43,78 @@
 #include "Std_Types.h"
 #include "Port_Cfg.h"
 
+/*******************************************************************************
+ *                              Module Data Types                              *
+ ********************************************************************************/
+
+/* Description: Data type for the symbolic name of a port pin */
+typedef uint8 Port_PinType;
+
+/* Description: Different port pin modes */
+typedef uint8 Port_PinModeType;
+
+/* Description: Possible directions of a port pin */
+typedef enum
+{
+  PORT_PIN_IN,
+  PORT_PIN_OUT
+} Port_PinDirectionType;
+
+typedef enum
+{
+  PORT_PIN_LEVEL_LOW,
+  PORT_PIN_LEVEL_HIGH
+} Port_PinLevelValue;
+
+typedef enum
+{
+  OFF,
+  PULL_UP,
+  PULL_DOWN
+} Port_InternalResistor;
+
+typedef enum
+{
+  PORT_PIN_MODE_ADC,
+  PORT_PIN_MODE_ALT1,
+  PORT_PIN_MODE_ALT2,
+  PORT_PIN_MODE_ALT3,
+  PORT_PIN_MODE_ALT4,
+  PORT_PIN_MODE_ALT5,
+  PORT_PIN_MODE_ALT6,
+  PORT_PIN_MODE_ALT7,
+  PORT_PIN_MODE_ALT8,
+  PORT_PIN_MODE_ALT9,
+  PORT_PIN_MODE_DIO,
+} Port_PinInitialMode;
+
+typedef struct
+{
+  uint8 port_num;                   /* PortA, PortB, PortC, .. */
+  uint8 pin_num;                    /* PA0, PA1, ... , PF3, PF4 */
+  Port_PinDirectionType direction;  /* PORT_PIN_IN, PORT_PIN_OUT */
+  Port_InternalResistor resistor;   /* OFF,PULL_UP,PULL_DOWN */
+  Port_PinLevelValue initial_value; /* PORT_PIN_LEVEL_LOW,PORT_PIN_LEVEL_HIGH */
+  Port_PinInitialMode initial_mode; /* e.g. PORT_PIN_MODE_DIO */
+  uint8 pin_dir_changeable;         /* STD_ON,STD_OFF */
+  uint8 pin_mode_changeable;        /* STD_ON,STD_OFF */
+} Port_ConfigPin;
+
+/* Array of Port_ConfigPin */
+typedef struct
+{
+  Port_ConfigPin Pin[PORT_CONFIGURED_PINS];
+} Port_ConfigType;
+
+typedef enum
+{
+  PORT_E_PARAM_PIN = 0x0A,              /* Invalid Port Pin ID requested */
+  PORT_E_DIRECTION_UNCHANGEABLE = 0x0B, /* Port Pin not configured as changeable */
+  PORT_E_INIT_FAILED = 0x0C,            /* API Port_Init service called with wrong parameter */
+  PORT_E_PARAM_INVALID_MODE = 0x0D,     /* API Port_SetPinMode service called when mode is unchangeable */
+  PORT_E_MODE_UNCHANGEABLE = 0x0E,      /* API Port_SetPinMode service called when mode is unchangeable */
+  PORT_E_UNINIT = 0x0F,                 /* API service called without module initialization*/
+  PORT_E_PARAM_POINTER = 0x10,          /*APIs called with a Null Pointer*/
+} DIO_DetErrors;
+
 #endif /* PORT_H_ */
--- Software/bsw/static/Mcal/Port/inc/Port_Cfg.h
+++ Software/bsw/static/Mcal/Port/inc/Port_Cfg.h
@@ -40,6 +40,53 @@
 /*                              Include headers                                */
 /*******************************************************************************/
 
+/*
+ * Module Version 1.0.0
+ */
+#define PORT_CFG_SW_MAJOR_VERSION              (1U)
+#define PORT_CFG_SW_MINOR_VERSION              (0U)
+#define PORT_CFG_SW_PATCH_VERSION              (0U)
+
+/*
+ * AUTOSAR Version 4.0.3
+ */
+#define PORT_CFG_AR_RELEASE_MAJOR_VERSION     (4U)
+#define PORT_CFG_AR_RELEASE_MINOR_VERSION     (0U)
+#define PORT_CFG_AR_RELEASE_PATCH_VERSION     (3U)
+
+/* Pre-compile option for Development Error Detect */
+#define PORT_DEV_ERROR_DETECT               (STD_ON)
+
+/* Pre-compile option for Version Info API */
+#define PORT_VERSION_INFO_API               (STD_ON)
+
+/* Pre-compile option for presence of Port_SetPinDirection API */
+#define PORT_SET_PIN_DIRECTION_API			(STD_ON)
+
+/* Pre-compile option for presence of Port_SetPinMode API */
+#define PORT_SET_PIN_MODE_API				(STD_ON)
+
+/* Number of PINS */
+#define PORT_CONFIGURED_PINS				(43U)
+
+/* Tiva-c Ports */
+#define PORT_PortA					(0U)
+#define PORT_PortB					(1U)
+#define PORT_PortC					(2U)
+#define PORT_PortD					(3U)
+#define PORT_PortE					(4U)
+#define PORT_PortF					(5U)
+
+/* Tiva-c Pins */
+#define PORT_Pin0                	(0U)
+#define PORT_Pin1                	(1U)
+#define PORT_Pin2                	(2U)
+#define PORT_Pin3                	(3U)
+#define PORT_Pin4                	(4U)
+#define PORT_Pin5                	(5U)
+#define PORT_Pin6                	(6U)
+#define PORT_Pin7                	(7U)
+
 #endif /* PORT_CFG_H_ */
 
 
